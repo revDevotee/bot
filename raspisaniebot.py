491ce4bb2282f5d08bc6166aca7d28d7ac44c4e6
revDevotee <rudakovr999@gmail.com>
2025-03-12 17:31:30 +0700

    РЕЛИЗ ЙОУ

--- raspisaniebot.py
+++ raspisaniebot.py
@@ -30,50 +30,75 @@
 # 503 Service Unavailable — сервер пока не готов обработать запрос. В ответе также может содержаться информация о том, когда сервис станет доступен.
 # 504 Gateway Timeout — эта ошибка означает, что обратный прокси не смог получить ответ за отведенное время (обычно — 60 секунд).
 
-from datetime import datetime
+import telebot as tbot
+import openpyxl as xl # сокращение
 from selenium import webdriver as wd
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import requests as r
 import time
-import re
-from bs4 import BeautifulSoup
+
+#чщщщщщ
+api_token = "7693781694:AAGMuB6q3w2FEZV90IWo5DHYz8ZhJCihBvo"
+bot = tbot.TeleBot(api_token)
+
+current_version = "Krivoy(NS)_1.0_Release" #NS-NotStable
 
 options = Options()
-options.add_argument('--headless')
+options.add_argument('--headless=chrome')
 
-EXPORT_FORMAT = 'xlsx'#формат экспорта таблицы
+def xlsx_search(file, message):
+    xlsx = xl.open(f"{file}", read_only=True)
+    sheet = xlsx.worksheets[4]# 9-ые квассы
 
+    cell = 20
 
-def Main():
+    #потом чрз цикл для оптимизации и возможности других классов
+    raspisanie = f'{sheet["A1"].value} (9К класс): \n \n'
+    for i in range (6):
+        raspisanie += f'{sheet[f"B{cell}"].value} | {sheet[f'G{cell}'].value}\n'    
+        cell += 2
+    bot.send_message(message.chat.id, raspisanie) 
+    cell = 20
+    #дебуг уээ
+    # print(f'{sheet["A1"].value} (9К класс)')
+    # print(f"{sheet['B20'].value} | {sheet['G20'].value}")
+    # print(f"{sheet['B22'].value} | {sheet['G22'].value}")
+    # print(f"{sheet['B24'].value} | {sheet['G24'].value}")
+    # print(f"{sheet['B26'].value} | {sheet['G26'].value}")
+    # print(f"{sheet['B28'].value} | {sheet['G28'].value}")
+    # print(f"{sheet['B30'].value} | {sheet['G30'].value}")
+    # print(f"{sheet['B32'].value} | {sheet['G32'].value}")
+
+#экспорт таблицы с сайтассылки
+def xlsx_export(EXPORT_FORMAT, url):
+    output_name = "raspisanie"
+    doc_id = url.replace("https://docs.google.com/spreadsheets/d/", "").split("/")[0]
+
+    schedule_url = f'https://docs.google.com/spreadsheets/d/{doc_id}/export?format={EXPORT_FORMAT}'
+    response = r.get(schedule_url)
+
+    with open(f'{output_name}.{EXPORT_FORMAT}', 'wb') as f:
+        f.write(response.content)
+        print(f"Данные успешно скачаны в {output_name}.{EXPORT_FORMAT}")
+
+def url_finder():
     try:
         school_url = "https://sh-kompleks-pokrovskij-r04.gosweb.gosuslugi.ru/glavnoe/raspisanie/"
         print("url ready")
 
-        driver = wd.Chrome()
+        driver = wd.Chrome(options=options)
         print("driver setted up")
 
         driver.get(school_url)
         print("window ready")
-        time.sleep(2)
+        time.sleep(1)#микро пауза
 
-        #БЛЯТЬ УРАААААААА, С 63 И 64 СТРОКА ВОЙДУТ В ЮБИЛЕЙНЫЕ СТРОКИ
-        schedule_link = driver.find_element(By.XPATH, '//*[@id="nc-block-df2d29ef0c9d7787f110009fa1561d60"]/div[2]/article/h3[2]/a[3]')
-        xlsx_sc = schedule_link.get_attribute('href') #containtment of schelude_link
+        schedule_link = driver.find_element(By.XPATH, '//*[@id="nc-block-df2d29ef0c9d7787f110009fa1561d60"]/div[2]/article/h3[2]/a[2]')#пипиряу чикиряу чо каво
+        xlsx_sc = schedule_link.get_attribute('href') #содержимое, крч ссылка
         print(f"schedule_link contant next - {xlsx_sc}")
-        
-        url = "https://docs.google.com/spreadsheets/d/1WhCpZaaugZ8WhLtbWM71Q6TrRM_oXkhD/edit?gid=963336423#gid=963336423"
 
-        doc_id = url.replace("https://docs.google.com/spreadsheets/d/", "").split("/")[0]
-
-        schedule_url = f'https://docs.google.com/spreadsheets/d/{doc_id}/export?format={EXPORT_FORMAT}'
-        response = r.get(schedule_url)
-
-        with open(f'output.{EXPORT_FORMAT}', 'wb') as f:
-            f.write(response.content)
-            print(f"Данные успешно скачаны в output.{EXPORT_FORMAT}")
-       
-        time.sleep(10)
+        xlsx_export('xlsx', xlsx_sc)
     except Exception as e:
         print('error: ', e)
     finally:
@@ -81,6 +106,21 @@ def Main():
         driver.quit()
         print("driver closed")
 
+@bot.message_handler(commands=['r'])
+def rasp(message):
+    xlsx_search("raspisanie.xlsx", message)
+    print(f"{message.from_user.username} использовал команду '/r'!")
+@bot.message_handler(commands=['ver'])
+def ver(message):
+    bot.send_message(message.chat.id, f'Текущая версия бота - {current_version}')
+
+def main():
+    url_finder()
+    bot.polling()
+
+    
+
+#чозабретто
 if __name__ == "__main__":
     print("init")
-    Main()
+    main()
