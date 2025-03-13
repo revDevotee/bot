b2b49023045162e7227c5a8314b8b851d031f1cc
revDevotee <rudakovr999@gmail.com>
2025-03-13 15:38:09 +0700

    1.4
    ээээ уууу, пофикшены уродские баги. ТЕПЕРЬ ОН ТРЕБУЕТ УКАЗАНИЯ ПУТИ ДЛЯ СОХРАНЕНИЯ ОБЯЗАТЕЛЬНО!!!!
    Также исправлен очередной косяк тех кто выкладывает расписание на сайт, и он перебирает варианты ссылок и ищет нужную!

--- raspisaniebot.py
+++ raspisaniebot.py
@@ -5,11 +5,15 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import requests as r
 import time
+import datetime
 
 api_token = "7693781694:AAGMuB6q3w2FEZV90IWo5DHYz8ZhJCihBvo"
 bot = tbot.TeleBot(api_token)
 
-current_version = "HolyMoly(NS)_1.2_Release" 
+current_version = "HolyMoly(NS)_1.4_Release" 
+
+curr_date = datetime.datetime.today() + datetime.timedelta(days=1)
+next_day = curr_date.strftime(f'%d.%m')
 
 options = Options()
 options.add_argument('--headless=chrome')
@@ -20,9 +24,10 @@ def xlsx_search(file, message):
 
     cell = 20
 
+    #later add multi classes(prob)
     raspisanie = f'{sheet["A1"].value} (9К класс): \n \n'
     for i in range (7):
-        raspisanie += f'{cell_check(sheet, cell, "B")} | {cell_check(sheet, cell, "G")} {cell_check(sheet, cell, "H")}\n'    
+        raspisanie += f'{cell_check(sheet, cell, "B")} | {cell_check(sheet, cell, "G")}  {cell_check(sheet, cell, "H")}\n'    
         cell += 2
     bot.send_message(message.chat.id, raspisanie) 
     cell = 20
@@ -41,15 +46,20 @@ def cell_check(sheet, cell, letter):
         return sheet[f'{letter}{cell}'].value
 
 def xlsx_export(EXPORT_FORMAT, url):
-    output_name = "raspisanie"
+    output_name = "rasp"
     doc_id = url.replace("https://docs.google.com/spreadsheets/d/", "").split("/")[0]
 
     schedule_url = f'https://docs.google.com/spreadsheets/d/{doc_id}/export?format={EXPORT_FORMAT}'
     response = r.get(schedule_url)
+    print("url responsed!")
 
-    with open(f'{output_name}.{EXPORT_FORMAT}', 'wb') as f:
+    #fix
+    #kill it with fire
+    #redact this path in linux!!!!
+    with open(f'C:/Timesword Dev/bot/rspbot/{output_name}.{EXPORT_FORMAT}', 'wb') as f:
+        print("Trying to write file")
         f.write(response.content)
-        print(f"Данные успешно скачаны в {output_name}.{EXPORT_FORMAT}")
+        print(f"{output_name}.{EXPORT_FORMAT} successfully ")
 
 def url_finder():
     try:
@@ -62,8 +72,13 @@ def url_finder():
         driver.get(school_url)
         print("window ready")
         time.sleep(1)
-
         schedule_link = driver.find_element(By.XPATH, '//*[@id="nc-block-df2d29ef0c9d7787f110009fa1561d60"]/div[2]/article/h3[2]/a[2]')
+        print(f"rasp date - {schedule_link.text.replace("Изменения в расписании старшей школы на ", "")}")
+        print(f"curr date - {next_day}")
+        if schedule_link.text.replace("Изменения в расписании старшей школы на ", "") != next_day:
+            schedule_link = driver.find_element(By.XPATH, '//*[@id="nc-block-df2d29ef0c9d7787f110009fa1561d60"]/div[2]/article/h3[2]/a[3]')
+            print(f"rasp date - {schedule_link.text.replace("Изменения в расписании старшей школы на ", "")}")
+            print(f"curr date - {next_day}")   
         xlsx_sc = schedule_link.get_attribute('href') 
         print(f"schedule_link contant next - {xlsx_sc}")
 
@@ -78,7 +93,7 @@ def url_finder():
 
 @bot.message_handler(commands=['r'])
 def rasp(message):
-    xlsx_search("raspisanie.xlsx", message)
+    xlsx_search("C:/Timesword Dev/bot/rspbot/rasp.xlsx", message)
     print(f"{message.from_user.username} использовал команду '/r'!")
 @bot.message_handler(commands=['ver'])
 def ver(message):
@@ -86,6 +101,7 @@ def ver(message):
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, f"Перед использованием бота, прочитайте <a href='https://telegra.ph/Polzovatelskoe-soglashenie-dlya-ispolzovaniya-bota-raspisaniervdvtbot-03-12'>пользовательское соглашение/политику конфиденциальности!</a>", parse_mode='HTML')
+
 
 def main():
     url_finder()
